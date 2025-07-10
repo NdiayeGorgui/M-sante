8362ea7bf8797b1bc188d60a528c5b3b46d6eeea
NdiayeGorgui <90470613+NdiayeGorgui@users.noreply.github.com>
2025-07-10 10:04:00 -0400

    first commit

---
+++ src/main/java/com/gogo/M_sante/MSanteApplication.java
@@ -0,0 +1,13 @@
+package com.gogo.M_sante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MSanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MSanteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gogo/M_sante/MSanteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gogo.M_sante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MSanteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
